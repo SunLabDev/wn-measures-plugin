6f1b25168eef58b0a52899ad5a7d31cfd7c525ab
RomainMazB <romainmazb@gmail.com>
2021-05-24 15:58:46 +0200

    Set translation keys

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,5 +1,20 @@
 <?php return [
     'plugin' => [
         'description' => 'Allows to create and increment any measures over any model you want.'
+    ],
+    'listened_events' => [
+        'active' => 'Active',
+        'event_type' => 'Event type',
+        'event_name' => 'Event name',
+        'model_to_watch' => 'Model to watch',
+        'on_logged_in_user' => 'Increment measure on logged in user',
+        'on_logged_in_user_desc' => 'Only supports Winter.User',
+        'measure_name' => 'Measure name',
+        'model_to_update' => 'Model to update',
+        'model_to_update_desc' => 'The model which the measure will be incremented',
+        'route_parameter' => 'Route parameter',
+        'route_parameter_desc' => 'The route parameter to retrieve the model',
+        'model_attribute' => 'Model attribute',
+        'model_attribute_desc' => 'The model attribute to match against route parameter',
     ]
 ];
--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,5 +1,20 @@
 <?php return [
     'plugin' => [
         'description' => 'Permet de créer de d\'incrémenter toute sorte de mesures sur n\'importe quel modèle.'
+    ],
+    'listened_events' => [
+        'active' => 'Actif',
+        'event_type' => 'Type d\'événement',
+        'event_name' => 'Nom de l\'événement',
+        'model_to_watch' => 'Modèle observé',
+        'on_logged_in_user' => 'Incrémenter la mesure de l\'utilisateur connecté',
+        'on_logged_in_user_desc' => 'Supporte uniquement Winter.User',
+        'measure_name' => 'Nom de la mesure',
+        'model_to_update' => 'Modèle à mettre à jour',
+        'model_to_update_desc' => 'Le modèle à mettre à jour',
+        'route_parameter' => 'Paramètre dans l\'url',
+        'route_parameter_desc' => 'Le paramètre pour retrouver le modèle',
+        'model_attribute' => 'Attribut du modèle',
+        'model_attribute_desc' => 'L\'attribut du modèle qui doit correspondre au paramètre de l\'url',
     ]
 ];
